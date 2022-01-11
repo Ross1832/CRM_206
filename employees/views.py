80b61954c5f2308eb45a0634071fa75918c0a700
Ross <komarostyslav@gmail.com>
2022-01-11 13:03:37 +0200

    start of the project

---
+++ employees/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+# def index(request):
+#     context = {
+#         'employees': 'there will be the list of employees'
+#     }
+#     return render(request)
