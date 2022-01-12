185cb54f73891898b9917b40da9ccb453a4ff444
Ross <komarostyslav@gmail.com>
2022-01-12 12:26:35 +0200

    rest_framework

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('employees.urls')),
+    path('', include('User.urls')),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
